d970e9219fd3845bf75d3d6692b155bd1e783d97
Elliott Rarden <ellman121@gamil.com>
2020-11-18 16:30:23 +0100

    Cleaned up the form behavior

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,16 +1,3 @@
 function createGame() {
     console.log("Create Game")
 }
-
-function joinGame(a, b, c) {
-    const gameID = $("#joinIDField").val();
-
-    if(gameID === "") {
-        return
-    }
-    window.location.href = "http://localhost:5500/join/" + $("#joinIDField").val()
-}
-
-$(document).ready(( )=> {
-    $("#joinForm").trigger("reset")
-})
