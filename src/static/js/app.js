9fe625e78e5271cbf8670c39c4927d5c174bc000
Elliott Rarden <ellman121@gamil.com>
2020-11-19 14:46:14 +0100

    Button now disables after being clicked (prevent spam)

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,8 +1,13 @@
 function createGame() {
-    var newID = ""
+    $("#createButton").prop("disabled", true)
     $.ajax({
         url: "http://localhost:5500/newSession"
     }).done((gameData) => {
         console.log(gameData.ID)
+        $("#joinIDField").val(gameData.ID)
     })
 }
+
+$(document).ready(() => {
+    $("#joinIDField").val(undefined)
+})
